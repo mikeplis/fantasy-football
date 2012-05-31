22c323bf5574c77786241f10b4ffaae175797a86
Michael Plis <michael.plis01@gmail.com>
2012-05-30 20:36:42 -0700

    changed setup.java to Driver.java

---
+++ src/Driver.java
@@ -0,0 +1,30 @@
+import java.net.UnknownHostException;
+import java.util.Set;
+
+import com.mongodb.Mongo;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoException;
+
+public class Driver {
+	
+	public static void main (String[] args) {
+		Mongo m;
+		try {
+			m = new Mongo();
+			DB db = m.getDB("ffdb");
+			DBCollection teams = db.getCollection("teams");
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MongoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
